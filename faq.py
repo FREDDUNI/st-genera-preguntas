56226306a2b917a3bbc503bfca5aa7a35d4bc9cf
FREDDUNI <57507434+FREDDUNI@users.noreply.github.com>
2023-11-07 16:50:00 -0500

    Update faq.py

--- faq.py
+++ faq.py
@@ -1,28 +1,26 @@
-#get a token: https://platform.openai.com/account/api-keys
-
-from langchain.llms import OpenAI
-from getpass import getpass
-from langchain.prompts import PromptTemplate
 import streamlit as st
+from langchain.llms import OpenAI
 
 st.title('Generador de preguntas relacionadas con el tema ingresado')
-st.caption('Introduzca en la consola la clave API como password antes y luego de ingresar el tema')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
-llm = OpenAI(
-    temperature=0.5,
-    model_name='gpt-3.5-turbo',
-    openai_api_key=openai_api_key,
-    max_tokens=150
-)
+if not openai_api_key.startswith('sk-'):
+    st.warning('Por favor ingrese su llave secreta de OpenAI', icon='⚠')
 
-plantilla = """
-Eres un experto en {topic}, ¿que información buscaría alguien que esté interesado en {topic}? como experto en {topic}, teniendo en cuenta la intención de búsqueda muéstrame las 5 mejores preguntas relacionadas al tema ingresado.
-""" 
+if openai_api_key.startswith('sk-'):
 
-pregunta =st.text_input("Escribe tu topic:")
-topic = pregunta
+    llm = OpenAI(
+        temperature=0.5,
+        model_name='gpt-3.5-turbo',
+        openai_api_key=openai_api_key,
+        max_tokens=150
+    )
+    topic = st.text_input("Escribe tu topic:")
+    
+    plantilla = """
+    Eres un experto en {topic}, ¿que información buscaría alguien que esté interesado en {topic}? como experto en {topic}, teniendo en cuenta la intención de búsqueda muéstrame las 5 mejores preguntas relacionadas al tema ingresado.
+    """ 
 
-respuesta = llm(plantilla.format(topic=topic))
-st.write(respuesta)
+    respuesta = llm(plantilla.format(topic=topic))
+    st.write(respuesta)
